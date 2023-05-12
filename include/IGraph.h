a4e63cf84eceb2927c07808e895eae532a5e14d5
kingjoram <alex.v.danilchenko@yandex.ru>
2023-05-12 20:11:33 +0300

    IGraph

---
+++ include/IGraph.h
@@ -0,0 +1,16 @@
+#ifndef SRC_IGRAPH_H_
+#define SRC_IGRAPH_H_
+
+struct IGraph {
+    virtual ~IGraph() {}
+
+    // Добавление ребра от from к to.
+    virtual void AddEdge(int from, int to) = 0;
+
+    virtual int VerticesCount() const = 0;
+
+    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+#endif // SRC_IGRAPH_H_
